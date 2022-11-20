5dfd0d282a461cd98205e283f23beff5525edec5
JeHee Yu <87363461+JeHeeYu@users.noreply.github.com>
2022-11-20 13:43:49 +0900

    어파인 변환

---
+++ Affine Transformation/Affine Transformation.cpp
@@ -0,0 +1,31 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	Mat src = imread("C:/opencv/ch02/Project1/lenna.bmp", IMREAD_GRAYSCALE);
+
+	Point2f srcPts[3], dstPts[3];
+
+	srcPts[0] = Point2f(0, 0);
+	srcPts[1] = Point2f(src.cols - 1, 0);
+	srcPts[2] = Point2f(src.cols - 1, src.rows - 1);
+	dstPts[0] = Point2f(50, 50);
+	dstPts[1] = Point2f(src.cols - 100, 100);
+	dstPts[2] = Point2f(src.cols - 50, src.rows - 50);
+
+	Mat M = getAffineTransform(srcPts, dstPts);
+
+	Mat dst;
+	warpAffine(src, dst, M, Size());
+
+
+	imshow("src", src);
+	imshow("dst", dst);
+	
+	waitKey();
+	
+}

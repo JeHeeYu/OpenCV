b7c85ecea8dc98576001ed3549163842d8860e40
JeHee Yu <87363461+JeHeeYu@users.noreply.github.com>
2022-11-24 14:17:43 +0900

    이벤트 처리 인터페이스

---
+++ EventInterface/EventInterface.cpp
@@ -0,0 +1,44 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+Mat img;
+Point ptOld;
+void on_mouse(int event, int x, int y, int flags, void*);
+
+int main()
+{
+	img = imread("lenna.bmp");
+
+	namedWindow("img");
+	setMouseCallback("img", on_mouse);
+
+	imshow("img", img);
+
+	waitKey();
+
+	return 0;
+}
+
+void on_mouse(int event, int x, int y, int flags, void*)
+{
+	switch (event) {
+	case EVENT_LBUTTONDOWN:
+		ptOld = Point(x, y);
+		cout << "EVENT_LBUTTONDOWN : " << x << ", " << y << endl;   
+		break;
+	case EVENT_LBUTTONUP:
+		cout << "EVENT_LBUTTONUP : " << x << ", " << y << endl;
+	case EVENT_MOUSEMOVE:
+		if (flags & EVENT_FLAG_LBUTTON) {
+			line(img, ptOld, Point(x, y), Scalar(0, 0, 0), 2);
+			imshow("img", img);
+			ptOld = Point(x, y);
+		}
+		break;
+	default:
+		break;
+	}
+}

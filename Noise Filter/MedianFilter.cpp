ff497f2eb6448b0897f695dda59ded38ccacf197
JeHee Yu <87363461+JeHeeYu@users.noreply.github.com>
2022-11-25 13:30:53 +0900

    미디언 필터

---
+++ Noise Filter/MedianFilter.cpp
@@ -0,0 +1,31 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	Mat src = imread("C:/opencv/ch02/Project1/lenna.bmp", IMREAD_GRAYSCALE);
+
+	int num = (int)(src.total() * 0.1);
+	
+	for (int i = 0; i < num; i++) {
+		int x = rand() % src.cols;
+		int y = rand() % src.rows;
+		src.at<uchar>(y, x) = (i % 2) * 255;
+	}
+
+	Mat dst1;
+	GaussianBlur(src, dst1, Size(), 1);
+
+	Mat dst2;
+	medianBlur(src, dst2, 3);
+
+
+	imshow("src", src);
+	imshow("dst1", dst1);
+	imshow("dst2", dst2);
+	waitKey();
+	
+}
